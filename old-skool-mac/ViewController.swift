8030ed600cba174a9d80acea5278549a84f42df8
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2015-11-22 21:02:45 -0500

    First run of code on simulator.

--- old-skool-mac/ViewController.swift
+++ old-skool-mac/ViewController.swift
@@ -12,10 +12,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mainLbl: UILabel!
     
+    var phrases = ["Booting from floppy... \n","Reading from disk... \n","Updating Registry...\n",".............\n","...................\n","Welcome, Jessica.   \n It is nice to see you again."]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainLbl.text = ""
+        for var x = 0; x < phrases.count; x++ {
+            var txt = mainLbl.text!
+            txt += phrases[x]
+            mainLbl.text = txt
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
